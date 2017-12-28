387318e3e309c7e48ef3dc7534b48b87dd2be5b4
Artyom Sadyrin <sadyryn@icloud.com>
2017-12-28 20:22:14 +0300

    Created Single View Project

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Artsiom Sadyryn on 12/28/17.
+//  Copyright © 2017 Artsiom Sadyryn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
