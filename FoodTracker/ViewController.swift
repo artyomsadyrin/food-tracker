97acf4477f90a59ce5330451c412c7fb65d2e30f
Artyom Sadyrin <sadyryn@icloud.com>
2018-01-01 11:57:13 +0300

    Added Outlet of Image View to code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var mealNameLabel: UILabel!
     
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
