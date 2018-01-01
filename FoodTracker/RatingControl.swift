d6a9683703d61041acb35955c5a894cd2a78562c
Artyom Sadyrin <sadyryn@icloud.com>
2018-01-01 14:02:53 +0300

    Created Custom View

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,23 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Artsiom Sadyryn on 1/1/18.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
