a687746d29c3c470d1045669208c3ae948553b84
Artyom Sadyrin <sadyryn@icloud.com>
2018-01-01 14:36:35 +0300

    Added support for UI Builder

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
     
     //MARK: Properties
     private var ratingButtons = [UIButton]()
     
     var rating = 0
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
 
     //MARK: Initialization
     
@@ -37,15 +49,22 @@ class RatingControl: UIStackView {
     
     private func setupButtons() {
         
-        for _ in 0..<5 {
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starCount {
             // Create the button
             let button = UIButton()
             button.backgroundColor = UIColor.red
             
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             // Setup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
@@ -56,6 +75,7 @@ class RatingControl: UIStackView {
             // Add the new button to the rating button array
             ratingButtons.append(button)
         }
+        
     }
 
 }
