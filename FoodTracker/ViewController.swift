ecea7afe3e87679d1e60f88d09502717f6e7b5c8
Artyom Sadyrin <sadyryn@icloud.com>
2018-01-02 11:16:44 +0300

    Finished doing implement a custom control

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -73,9 +73,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         present(imagePickerController, animated: true, completion: nil)
     }
     
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-    
 }
 
